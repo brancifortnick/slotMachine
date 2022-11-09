eed055295fc42d2565a5a3698a69527da997e3c7
brancifortnick <brancifortnick@yahoo.com>
2022-11-08 19:06:21 -0500

    starting_slot_logic

---
+++ index.js
@@ -0,0 +1,21 @@
+const sammy = (nums) => {
+	let intRand = Math.floor(Math.random() * nums)
+	if (intRand === 0) {
+		intRand += 1
+	}
+	if (intRand >= 4) {
+		return [intRand, intRand, intRand].join(' ')
+	}
+	return intRand
+}
+
+setTimeout(function() {
+	console.log(sammy(5, 5, 5))
+}, 175)
+setTimeout(function() {
+	console.log(sammy(5, 5, 5))
+}, 300)
+
+setTimeout(function() {
+	console.log(sammy(3, 3, 3))
+}, 500)
